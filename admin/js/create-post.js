46ece1f382463304614e8fab926843b229d8cc08
Kevin Leskinen <kevnl_95@hotmail.com>
2020-12-18 17:03:58 +0100

    Added error handling to admin

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,29 +1,39 @@
 let form = document.getElementById('create-post-form');
+let formTitle = document.getElementById('input-title');
+let formAuthor = document.getElementById('input-author');
+let formContent = document.getElementById('post-content');
+let formDefaultTag = document.getElementById('default');
+let errorHandler = document.getElementById('error-handler');
+
 form.addEventListener('submit', createPost);
 
 async function createPost(e) {
     e.preventDefault();
 
-    let formData = new FormData(this);
+    if(formTitle.value.trim() === "" || formAuthor.value.trim() === "" || formContent.value.trim() === "" || formDefaultTag.selected) {
+        errorHandler.innerText = "Fill in fields and select an actual tag";
+    } else {
+        let formData = new FormData(this);
 
-    let object = {
-        tags: formData.getAll('tags'),
-        title: formData.get("input-title"),
-        author: formData.get("input-author"),
-        content: formData.get("textarea-content")
-    }
-    
-    try {
-        await fetch('http://localhost:3000/posts/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(object)
-        });
+        let object = {
+            tags: formData.getAll('tags'),
+            title: formData.get("input-title"),
+            author: formData.get("input-author"),
+            content: formData.get("textarea-content")
+        }
+        
+        try {
+            await fetch('http://localhost:3000/posts/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            });
+            window.location.replace('index.html');
 
-        window.location.replace('index.html');
-    } catch (e) {
-        throw new Error(e);
+        } catch (e) {
+            throw new Error(e);
+        }
     }
 }
--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -3,16 +3,23 @@ window.onload = function() {
     updatePost();
 }
 
+let formTitle = document.getElementById('input-title');
+let formAuthor = document.getElementById('input-author');
+let formContent = document.getElementById('post-content');
+let formTags = document.getElementById('post-tags')
+let formDefaultTag = document.getElementById('default');
+let errorHandler = document.getElementById('error-handler');
+
 async function prefillForm() {
     let urlParams = new URLSearchParams(window.location.search);
 
     try {
         let response = await fetch('http://localhost:3000/posts/' + urlParams.get('id'));
         let data = await response.json();
-        document.getElementById('input-title').value = data.title;
-        document.getElementById('input-author').value = data.author;
-        document.getElementById('post-content').value = data.content;
-        document.getElementById('post-tags').value = data.tags;
+        formTitle.value = data.title;
+        formAuthor.value = data.author;
+        formContent.value = data.content;
+        formTags.value = data.tags;
 
     } catch (message) {
         throw new Error(message);
@@ -25,27 +32,31 @@ function updatePost() {
 
     form.addEventListener("submit", async function (e) {
         e.preventDefault();
-        
-        let formData = new FormData(this);
-        let object = {
-            tags: formData.getAll('tags'),
-            title: formData.get('input-title'),
-            author: formData.get('input-author'),
-            content: formData.get('textarea-content')
-        }
 
-        try {
-            await fetch('http://localhost:3000/posts/' + urlParams.get('id') , {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            });
-            
-            window.location.replace('index.html');
-        } catch (error) {
-            throw new Error(error);
+        if(formTitle.value.trim() === "" || formAuthor.value.trim() === "" || formContent.value.trim() === "" || formDefaultTag.selected) {
+            errorHandler.innerText = "Fill in fields and select an actual tag";
+        } else {
+            let formData = new FormData(this);
+            let object = {
+                tags: formData.getAll('tags'),
+                title: formData.get('input-title'),
+                author: formData.get('input-author'),
+                content: formData.get('textarea-content')
+            }
+    
+            try {
+                await fetch('http://localhost:3000/posts/' + urlParams.get('id') , {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(object)
+                });
+                
+                window.location.replace('index.html');
+            } catch (error) {
+                throw new Error(error);
+            } 
         }
     });
 }
